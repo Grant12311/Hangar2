3a27006d37a72dce639129fbacb0c07d9d5460e3
Grant Joseph <grantj150@yahoo.com>
2020-10-15 20:27:59 -0400

    Config::title is now a char* (was const char*)

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ namespace Hangar
 {
     struct Config
     {
-        const char* title = "Hangar2 Window";
+        char* title = "Hangar2 Window";
         unsigned int height = 480, width = 640, borderWidth = 0, depth = 0;
         bool closeOnEscape = true;
         bool fullscreen = false;
